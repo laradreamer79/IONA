77dc20186a972abcbecc81bdc92d9a3a62707a4c
laradreamer79 <laradreamer79@gmail.com>
2026-05-12 18:24:19 +0300

    Initial Unity project

---
+++ Assets/DiverMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class DiverMovement : MonoBehaviour
+{
+    public float speed = 3f;
+    public float turnSpeed = 120f;
+    public float verticalSpeed = 2f;
+
+    void Update()
+    {
+        float move = Input.GetAxis("Vertical");   // W/S
+        float turn = Input.GetAxis("Horizontal"); // A/D
+
+        transform.Translate(Vector3.forward * move * speed * Time.deltaTime);
+        transform.Rotate(Vector3.up * turn * turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Space))
+            transform.Translate(Vector3.up * verticalSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            transform.Translate(Vector3.down * verticalSpeed * Time.deltaTime);
+    }
+}
